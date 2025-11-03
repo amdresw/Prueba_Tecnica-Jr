599455855cc5f310362ef205971a712687078667
andres <132904420+amdresw@users.noreply.github.com>
2025-11-03 01:33:29 -0500

    feat: :tada: Estructura base
    Inicializa estructura base del proyecto MVC en .NET 8

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FacturacionMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
