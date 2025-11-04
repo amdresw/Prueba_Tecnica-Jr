365a03a12e8ef94ae6c41a89d4504dc1a752f176
andres <132904420+amdresw@users.noreply.github.com>
2025-11-03 20:20:06 -0500

    feat: :sparkles: Facturas
    implementa controlador y vistas para Facturas

---
+++ Controllers/FacturasController.cs
@@ -0,0 +1,164 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FacturacionMVC.Models;
+
+namespace FacturacionMVC.Controllers
+{
+    public class FacturasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FacturasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Facturas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Facturas.Include(f => f.Cliente).Include(f => f.Emisor);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Facturas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas
+                .Include(f => f.Cliente)
+                .Include(f => f.Emisor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+
+            return View(factura);
+        }
+
+        // GET: Facturas/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Apellidos");
+            ViewData["IdEmisor"] = new SelectList(_context.Emisores, "Id", "Direccion");
+            return View();
+        }
+
+        // POST: Facturas/Create
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdCliente,IdEmisor,Fecha,TotalFactura")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(factura);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Apellidos", factura.IdCliente);
+            ViewData["IdEmisor"] = new SelectList(_context.Emisores, "Id", "Direccion", factura.IdEmisor);
+            return View(factura);
+        }
+
+        // GET: Facturas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas.FindAsync(id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Apellidos", factura.IdCliente);
+            ViewData["IdEmisor"] = new SelectList(_context.Emisores, "Id", "Direccion", factura.IdEmisor);
+            return View(factura);
+        }
+
+        // POST: Facturas/Edit/5
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdCliente,IdEmisor,Fecha,TotalFactura")] Factura factura)
+        {
+            if (id != factura.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(factura);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacturaExists(factura.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Apellidos", factura.IdCliente);
+            ViewData["IdEmisor"] = new SelectList(_context.Emisores, "Id", "Direccion", factura.IdEmisor);
+            return View(factura);
+        }
+
+        // GET: Facturas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas
+                .Include(f => f.Cliente)
+                .Include(f => f.Emisor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+
+            return View(factura);
+        }
+
+        // POST: Facturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var factura = await _context.Facturas.FindAsync(id);
+            if (factura != null)
+            {
+                _context.Facturas.Remove(factura);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacturaExists(int id)
+        {
+            return _context.Facturas.Any(e => e.Id == id);
+        }
+    }
+}
--- obj/Debug/net8.0/FacturacionMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/FacturacionMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturacionMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e104217c06ac15db06157d5f6b69fd92cbdd63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e91d01f5dbddc4dc67eb419a9f441b4cc0f482c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturacionMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturacionMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
